ce3afb4b95153f766c8c9a61ca9b3003d7124a06
bleedingedge121 <kavishjpr@gmail.com>
2026-03-24 19:01:57 +0530

    removed wallpaper

--- fish/.config/fish/conf.d/abbreviations.fish
+++ fish/.config/fish/conf.d/abbreviations.fish
@@ -6,3 +6,4 @@ abbr --add -- upg 'sudo pacman -Syu && yay -Syu'
 abbr --add -- f fastfetch
 abbr --add -- dots 'cd ~/dotfiles;clear'
 abbr --add -- olstart 'OLLAMA_KEEP_ALIVE=0 OLLAMA_NUM_PARALLEL=1 ollama serve'
+abbr --add -- s 'yay -S'
