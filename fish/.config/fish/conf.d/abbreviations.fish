8bd7463c13841af1bdf99fb49eb3de9e1507eb47
bleedingedge121 <kavishjpr@gmail.com>
2026-03-29 14:48:14 +0530

    fish abbr

--- fish/.config/fish/conf.d/abbreviations.fish
+++ fish/.config/fish/conf.d/abbreviations.fish
@@ -7,4 +7,4 @@ abbr --add -- f fastfetch
 abbr --add -- dots 'cd ~/dotfiles;clear'
 abbr --add -- olstart 'OLLAMA_KEEP_ALIVE=0 OLLAMA_NUM_PARALLEL=1 ollama serve'
 abbr --add -- s 'yay -S'
-abbr --add -- i 'yay '
+abbr --add -- i 'yay'
